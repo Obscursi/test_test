13b24e68af02b04aeff6825ff4582147d561d6da
Mat <sabben@insa-toulouse.fr>
2026-07-01 15:12:17 +0200

    changement code Enigme Aruco

--- Enigmas/ArucoEnigma.js
+++ Enigmas/ArucoEnigma.js
@@ -5,32 +5,16 @@ export class ArucoEnigma extends Enigma {
         // On appelle le constructeur de la classe parente (Enigma)
         super("enigme_vrai-faux", "Aruco");
 
-        // Les 4 lettres qui doivent être vues en même temps
-        //this.lettresRequises = ["P", "I", "E", "D"];
     }
 
     // Le GameEngine appelle cette fonction 15 fois par seconde
     checkCondition(playerState) {
         if (this.estResolu) return;
-
-        const gestesActuels = playerState.gestures; // Ex: ["P", "Thumb_Up", "I"]
-
-        // if we don't see enough hands to form the word it is not useful to look further
-        if (gestesActuels.length < this.lettresRequises.length) {
-            return;
-        }
-
-        // On vérifie que TOUTES les lettres requises sont dans le tableau des gestes actuels
-        const toutesPresentes = this.lettresRequises.every(lettre => gestesActuels.includes(lettre));
-
-        if (toutesPresentes) {
-            this.unlock();
-        }
     }
 
     onSuccess() {
         // C'est ici que tu pourras dire au UIManager d'afficher la suite !
-        console.log("🔓 Le cadenas LSF est ouvert !");
+        console.log("🔓 Le cadenas Aruco est ouvert !");
 
         // Exemple d'action : 
         // document.getElementById("panel-aruco").style.display = "block";
